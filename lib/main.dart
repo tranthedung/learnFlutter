15e6fe501aca03e0fd0efe991753af05c94d1d24
tranthedung <xdungx123@gmail.com>
2023-06-07 16:40:41 +0700

    07/07/2002

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+  // Add this
+  void getNext(){
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;    //<-- add this
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,  // add this
+        children: [
+          Text('A random AWESOME idea'),
+          BigCard(pair: pair,),  // ← Example change.
+          ElevatedButton(
+              onPressed:(){
+                appState.getNext(); // <- this instead of print().
+              },
+              child: Text('Next'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+  final WordPair pair;
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);  // add this
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,   // and also this
+        child: Padding(
+        padding: const EdgeInsets.all(20),
+
+        // making the following change
+        child: Text(
+            pair.asLowerCase,
+            style: style,
+            semanticsLabel: "${pair.first} ${pair.second}",
+          ),
+        ),
+    );
+  }
+}
